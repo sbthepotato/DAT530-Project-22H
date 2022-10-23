e47cb8ccdb1c321b7e84a1833badcc6ca54ed12f
Rehab Al Zurkani <88684173+RihabHashim@users.noreply.github.com>
2022-10-23 19:56:21 +0200

    Add files via upload

---
+++ 2.aisle block/t2_pre.m
@@ -0,0 +1,15 @@
+function [fire, transition] = t2_pre (transition)
+
+global global_info
+ 
+if eq(global_info.CASE, 1), 
+    % Case = 1: t1 is preffered,
+    % In this case, block t2, if t1 is enabled and same for t3
+    fire = not(is_enabled('t1'));
+elseif eq(global_info.CASE, 3), 
+    fire = not(is_enabled('t3'));
+else
+    % Case-2: t2 is preferred,
+    % thus, fire t2, if it is enabled
+    fire = 1;
+end
