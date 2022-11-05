a4a5be1ba915d61a6bcf12ee639728be460ac22d
Stephan Brandasu <Stephanbrandasu@gmail.com>
2022-11-05 17:47:27 +0100

    first basic fully working model complete
    now I need to create variations, make the print nicer and comment all the code

--- simulation/COMMON_POST.m
+++ simulation/COMMON_POST.m
@@ -2,8 +2,91 @@ function [] = COMMON_POST(trans)
 
 global global_info
 
-if strcmp(trans.name, 'tL01'),
+% re-enable the aisle transition after the side transition has finished
+% firing. 
+if strcmp(trans.name, 'tL01')
     global_info.A01 = 1;
-elseif strcmp(trans.name, 'tR01'),
+elseif strcmp(trans.name, 'tR01')
     global_info.A01 = 1;
-end;
+elseif strcmp(trans.name, 'tL02')
+    global_info.A02 = 1;
+elseif strcmp(trans.name, 'tR02')
+    global_info.A02 = 1;
+elseif strcmp(trans.name, 'tL03')
+    global_info.A03 = 1;
+elseif strcmp(trans.name, 'tR03')
+    global_info.A03 = 1;
+elseif strcmp(trans.name, 'tL04')
+    global_info.A04 = 1;
+elseif strcmp(trans.name, 'tR04')
+    global_info.A04 = 1;
+elseif strcmp(trans.name, 'tL05')
+    global_info.A05 = 1;
+elseif strcmp(trans.name, 'tR05')
+    global_info.A05 = 1;
+elseif strcmp(trans.name, 'tL06')
+    global_info.A06 = 1;
+elseif strcmp(trans.name, 'tR06')
+    global_info.A06 = 1;
+elseif strcmp(trans.name, 'tL07')
+    global_info.A07 = 1;
+elseif strcmp(trans.name, 'tR07')
+    global_info.A07 = 1;
+elseif strcmp(trans.name, 'tL08')
+    global_info.A08 = 1;
+elseif strcmp(trans.name, 'tR08')
+    global_info.A08 = 1;
+elseif strcmp(trans.name, 'tL09')
+    global_info.A09 = 1;
+elseif strcmp(trans.name, 'tR09')
+    global_info.A09 = 1;
+elseif strcmp(trans.name, 'tL10')
+    global_info.A10 = 1;
+elseif strcmp(trans.name, 'tR10')
+    global_info.A10 = 1;
+elseif strcmp(trans.name, 'tL11')
+    global_info.A11 = 1;
+elseif strcmp(trans.name, 'tR11')
+    global_info.A11 = 1;
+elseif strcmp(trans.name, 'tL12')
+    global_info.A12 = 1;
+elseif strcmp(trans.name, 'tR12')
+    global_info.A12 = 1;
+elseif strcmp(trans.name, 'tA01')
+    global_info.A01 = 0;
+elseif strcmp(trans.name, 'tA02')
+    global_info.A02 = 0;
+    global_info.A01 = 1;
+elseif strcmp(trans.name, 'tA03')
+    global_info.A03 = 0;
+    global_info.A02 = 1;
+elseif strcmp(trans.name, 'tA04')
+    global_info.A04 = 0;
+    global_info.A03 = 1;
+elseif strcmp(trans.name, 'tA05')
+    global_info.A05 = 0;
+    global_info.A04 = 1;
+elseif strcmp(trans.name, 'tA06')
+    global_info.A06 = 0;
+    global_info.A05 = 1;
+elseif strcmp(trans.name, 'tA07')
+    global_info.A07 = 0;
+    global_info.A06 = 1;
+elseif strcmp(trans.name, 'tA08')
+    global_info.A08 = 0;
+    global_info.A07 = 1;
+elseif strcmp(trans.name, 'tA09')
+    global_info.A09 = 0;
+    global_info.A08 = 1;
+elseif strcmp(trans.name, 'tA10')
+    global_info.A10 = 0;
+    global_info.A09 = 1;
+elseif strcmp(trans.name, 'tA11')
+    global_info.A11 = 0;
+    global_info.A10 = 1;
+elseif strcmp(trans.name, 'tA12')
+    global_info.A12 = 0;
+    global_info.A11 = 1;
+else
+    % nothing
+end
--- simulation/main.m
+++ simulation/main.m
@@ -1,7 +1,11 @@
 clear all; clc;
 global global_info;
 
-global_info.STOP_AT = 100;
+global_info.STOP_AT = 60;
+
+global_info.cr = {'A','B','C','D','E','F','G','H','I','J','K','L' ...
+    'Z','Y','X','W','V','U','T','S','R','Q','P','O'};
+global_info.cr_index = 0;
 
 pns = pnstruct('main_pn_pdf');
 
@@ -19,17 +23,23 @@ global_info.A11 = 1;
 global_info.A12 = 1;
 
 % token count
-dyn.m0 = {'pStart',3};
+dyn.m0 = {'pStart', 24};
 % firing times
-dyn.ft = {'allothers', 2};
-% transition priority
-dyn.ip = {'tL01',1,'tR01',1};
+dyn.ft = {'allothers', 1};
+% transition priority, prioritise side ones to make sure colours go down
+% their pathway
+dyn.ip = {'tL01',1,'tL02',1,'tL03',1,'tL04',1,'tL05',1,'tL06',1, ...
+    'tL07',1,'tL08',1,'tL09',1,'tL10',1,'tL11',1,'tL12',1, ...
+    'tR01',1,'tR02',1,'tR03',1,'tR04',1,'tR05',1,'tR06',1, ...
+    'tR07',1,'tR08',1,'tR09',1,'tR10',1,'tR11',1,'tR12',1
+};
 
 pni = initialdynamics(pns, dyn);
 
 sim = gpensim(pni);
 
-plotp(sim, {'pA00', ...
+% the whole model
+plotp(sim, { ...
     'pL01','pA01','pR01', ...
     'pL02','pA02','pR02', ...
     'pL03','pA03','pR03', ...
@@ -42,6 +52,28 @@ plotp(sim, {'pA00', ...
     'pL10','pA10','pR10', ...
     'pL11','pA11','pR11', ...
     'pL12','pA12','pR12'
-    });
+});
+
+%{
+% only the aisle
+plotp(sim, { 'pA00', ...
+    'pA01','pA02','pA03', 'pA04','pA05','pA06', ...
+    'pA07','pA08','pA09','pA10','pA11','pA12'
+});
+
+% only the right side
+plotp(sim, {...
+    'pR01','pR02','pR03', 'pR04','pR05','pR06', ...
+    'pR07','pR08','pR09','pR10','pR11','pR12'
+});
+
+% only the left side
+plotp(sim, {...
+    'pL01','pL02','pL03', 'pL04','pL05','pL06', ...
+    'pL07','pL08','pL09','pL10','pL11','pL12'
+});
+%}
 
 prnss(sim);
+%prncolormap(sim);
+%prnfinalcolors(sim);
